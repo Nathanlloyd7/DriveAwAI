7a10a32237b1934543bccbcb41e87766a221a10f
Nathanlloyd7 <35963461+Nathanlloyd7@users.noreply.github.com>
2019-09-12 17:07:15 +0100

    Update RCSpeedPanel.py
    RC speed panel, figured out how to use methods on a button command, overlooked the fact I did not need to call it with brackets.
    
    Updating speed as shown here was just to test this, as updating like this for the full app would just trigger the motors rather than update a speed variable. Currently working on RC class, just getting set speed functions to fix this problem

--- RCSpeedPanel.py
+++ RCSpeedPanel.py
@@ -1,5 +1,8 @@
 #Speed panel to edit the speed of the rc.
+#12/08/19
+import explorerhat 
 from tkinter import *
+
 
 class RCSpeedPanel:
     def __init__(self, root, frame):
@@ -7,11 +10,26 @@ class RCSpeedPanel:
         self.frame = frame
 #Speed Frame
         Label(self.frame, text="Speed:").grid(row=0, column=0, padx=10, pady=2)
-        self.slowBtn = Button(self.frame, text="1", command=lambda:print("Speed 0.25%"))
+        self.slowBtn = Button(self.frame, text="1", command= speed1)
         self.slowBtn.grid(row=1, column=0, padx=10, pady=2)
-        self.medBtn = Button(self.frame, text="2", command=lambda:print("Speed 0.50%"))
+        self.medBtn = Button(self.frame, text="2", command=speed2 )
         self.medBtn.grid(row=2, column=0, padx=10, pady=2)
-        self.fastBtn = Button(self.frame, text="3", command=lambda:print("Speed 0.75%"))
+        self.fastBtn = Button(self.frame, text="3", command = speed3)
         self.fastBtn.grid(row=3, column=0, padx=10, pady=2)
         self.offBtn = Button(self.frame, text="Handbreak", command=lambda:print("Hand Break"))
         self.offBtn.grid(row=4, column=0, padx=10, pady=2)
+        
+global speedint
+speedint = 100
+def speed1():
+    explorerhat.motor.one.forwards(speedint *0.333)
+    explorerhat.motor.two.forward(speedint *0.333)
+    
+def speed2():
+    explorerhat.motor.one.forwards(75)    #so updating speed like this is wrong
+    explorerhat.motor.two.forward(75)     #if i do like this then it will make it go  
+    
+def speed3():
+    explorerhat.motor.one.forwards(speedint ) #wereas i just want a var to be updated
+    explorerhat.motor.two.forward(speedint )
+    
