1d6efb56e6a87a170334f201b3ef79779a135387
Nathanlloyd7 <35963461+Nathanlloyd7@users.noreply.github.com>
2019-09-12 18:43:52 +0100

    Update RCSpeedPanel.py
    the commands to change speed work, as tested with the offBtn instead performing the toStr method, speed is just not however communicated over to the direction panel

--- RCSpeedPanel.py
+++ RCSpeedPanel.py
@@ -1,35 +1,24 @@
 #Speed panel to edit the speed of the rc.
 #12/09/19
-import explorerhat 
+from RCCar import *
 from tkinter import *
 
 
 class RCSpeedPanel:
     def __init__(self, root, frame):
+        rc = RCCar(True, 50, 50)
         self.root = root
         self.frame = frame
 #Speed Frame
         Label(self.frame, text="Speed:").grid(row=0, column=0, padx=10, pady=2)
-        self.slowBtn = Button(self.frame, text="1", command= speed1)
+        self.slowBtn = Button(self.frame, text="1", command= rc.set_low_spd)
         self.slowBtn.grid(row=1, column=0, padx=10, pady=2)
-        self.medBtn = Button(self.frame, text="2", command=speed2 )
+        self.medBtn = Button(self.frame, text="2", command= rc.set_med_spd )
         self.medBtn.grid(row=2, column=0, padx=10, pady=2)
-        self.fastBtn = Button(self.frame, text="3", command = speed3)
+        self.fastBtn = Button(self.frame, text="3", command = rc.set_high_spd)
         self.fastBtn.grid(row=3, column=0, padx=10, pady=2)
-        self.offBtn = Button(self.frame, text="Handbreak", command=lambda:print("Hand Break"))
+        self.offBtn = Button(self.frame, text="Handbreak", command=lambda:print(rc.motors_spd_toStr()))
         self.offBtn.grid(row=4, column=0, padx=10, pady=2)
-        
-global speedint
-speedint = 100
-def speed1():
-    explorerhat.motor.one.forwards(speedint *0.333)
-    explorerhat.motor.two.forward(speedint *0.333)
     
-def speed2():
-    explorerhat.motor.one.forwards(75)    #so updating speed like this is wrong
-    explorerhat.motor.two.forward(75)     #if i do like this then it will make it go  
-    
-def speed3():
-    explorerhat.motor.one.forwards(speedint ) #wereas i just want a var to be updated
-    explorerhat.motor.two.forward(speedint )
-    
+
+
