85f6605cfd2ea72a4992f35e87e8f6fcefebb0c7
Nathanlloyd7 <35963461+Nathanlloyd7@users.noreply.github.com>
2019-10-13 21:27:17 +0100

    Update RCVideoPanel.py
    Working video class

--- RCVideoPanel.py
+++ RCVideoPanel.py
@@ -1,29 +1,31 @@
-#view the live video from the picamera
+import numpy as np
+import cv2
+import tkinter as tk
 from tkinter import *
-from RCButtons import *
+from PIL import Image, ImageTk
 
+#videostream RPiCam2
 class RCVideoPanel:
-    #As I've been adapting code from a tkinter tutorial and have not tried using open cv yet for video streaming into the gui
-    #I will be leaving this panel as is, this is the only class that uses the RCButtons class which is displayed in the tutorial
-    def __init__(self, root, frame):
+    def __init__(self, root, frame): 
         self.root = root
         self.frame = frame
+#Video Frame
+        self.imageFrame = Frame(self.frame, width=600, height=500)
+        self.imageFrame.grid(row=0, column=0, padx=10, pady=2)
+#Capture video frames
+        self.lmain = tk.Label(self.imageFrame) #attach image to label
+        self.lmain.grid(row=0, column=0)
+        self.capture = cv2.VideoCapture(0)
+        self.show_frame()  
+        
+    def show_frame(self):
+        _, frame = self.capture.read()
+        frame = cv2.flip(frame, 1)
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        self.lmain.imgtk = imgtk
+        self.lmain.configure(image=imgtk)
+        self.lmain.after(10, self.show_frame) #takes a photo but doesn't fully work
 
-		#Right Frame and its contents/ needs biggest edit open cv for video
-        self.circleCanvas = Canvas(self.frame, width=100, height=100, bg='white')
-        self.circleCanvas.grid(row=0, column=0, padx=10, pady=2)
 
-        self.btnFrame = Frame(self.frame, width=200, height=10, borderwidth=1)
-        self.btnFrame.grid(row=1, column=0, padx=10, pady=2)
-
-
-        self.redBtn = RCButtons(self.btnFrame, "red", self.makeCircle)
-        self.yellowBtn = RCButtons(self.btnFrame, "yellow", self.makeCircle)
-        self.greenBtn = RCButtons(self.btnFrame, "green", self.makeCircle)
-
-        self.colorLog = Text(self.frame, width = 30, height = 10, takefocus=0)
-        self.colorLog.grid(row=3, column=0, padx=10, pady=2)
-
-    def makeCircle(self, color):
-        self.circleCanvas.create_oval(20, 20, 80, 80, width=0, fill=color)
-        self.colorLog.insert(0.0, color.capitalize() + "\n")
