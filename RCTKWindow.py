03752ff935f6fa1cb41a97bb08f63b3f8e5113f8
Nathanlloyd7 <35963461+Nathanlloyd7@users.noreply.github.com>
2019-10-16 22:00:56 +0100

    Update RCTKWindow.py

--- RCTKWindow.py
+++ RCTKWindow.py
@@ -9,16 +9,17 @@ from RCMenuBar import RCMenuBar
 class RCTKWindow(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self) #Makes the window
-        menubar = RCMenuBar(self)
-        self.config(menu=menubar)
-        self.minsize(300,360)
+        self.menubar = RCMenuBar(self)
+        self.config(menu=self.menubar)
+        self.minsize(950,530)
+        self.geometry('950x530+0+0')
         self.tk.call('wm', 'iconphoto', self._w, tk.PhotoImage(file='realcar1.png'))
         self.wm_title("RC App") #Makes the title that will appear in the top left
         self.config(background = "Cyan")
         self.leftFrame = Frame(self, width=200, height = 600)
-        self.leftFrame.grid(row=0, column=0, padx=10, pady=2)
+        self.leftFrame.grid(row=0, column=0, padx=10, pady=10)
         self.centralFrame = Frame(self, width=200, height = 600)
-        self.centralFrame.grid(row=0, column=1, padx=10, pady=2)
+        self.centralFrame.grid(row=0, column=1, padx=10, pady=10)
         self.leftPanel = RCMovementPanel(self, self.leftFrame)
         self.centralFrame = RCVideoPanel(self, self.centralFrame)
 
