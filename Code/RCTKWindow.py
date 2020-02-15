6c5529539ba63b024adaca7d384f1998402e6d31
Nathanlloyd7 <35963461+Nathanlloyd7@users.noreply.github.com>
2020-02-15 17:17:14 +0000

    Update RCTKWindow.py

--- Code/RCTKWindow.py
+++ Code/RCTKWindow.py
@@ -11,12 +11,13 @@ from Code.RCPassword import RCPassword
 class RCTKWindow(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self) #Makes the window
+        dlpath = os.getcwd()
         self.passW = RCPassword()
         self.menubar = RCMenuBar(self)
         self.config(menu=self.menubar)
         self.minsize(950,530)
         self.geometry('950x530+0+0')
-        self.tk.call('wm', 'iconphoto', self._w, tk.PhotoImage(file='/Images/realcar1.png'))
+        self.tk.call('wm', 'iconphoto', self._w, tk.PhotoImage(file= dlpath+'/Code/Images/realcar1.png'))
         self.wm_title("Drive AwAI") #Makes the title that will appear in the top left
         self.config(background = "Cyan")
         self.leftFrame = Frame(self, width=200, height = 600)
